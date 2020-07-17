618f5d2a0bac8e3f8d9473ff9c4f3402385cba86
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-07-17 23:54:05 +0300

    my first Telegram bot

--- src/main/java/com/home/mytelegram_bot/MyTelegramBot.java
+++ src/main/java/com/home/mytelegram_bot/MyTelegramBot.java
@@ -1,58 +1,59 @@
 package com.home.mytelegram_bot;
 
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class MyTelegramBot extends TelegramWebhookBot {
-    private String webHookPath;
-    private String botUserName;
-    private String botToken;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MyTelegramBot(DefaultBotOptions botOptions) {
-        super(botOptions);
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+
+        SendMessage response = new SendMessage();
+        response.setText("Hello, " + message.getFrom().getFirstName() + ".Welcome to the telegram bot :)\n" +
+                "You have send me: " + message.getText());
+        response.setChatId(message.getChatId());
+        response.setReplyMarkup(getMainMenu());
+        try {
+            execute(response);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return botUserName;
+        return "katorzhin_bot";
     }
 
     @Override
     public String getBotToken() {
-        return botToken;
+        return "<botToken>";
     }
 
-    @Override
-    public String getBotPath() {
-        return webHookPath;
-    }
+    private ReplyKeyboardMarkup getMainMenu() {
+        ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add("Button 1");
+        row1.add("Button 2");
 
-    @Override
-    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        if (update.getMessage() != null && update.getMessage().hasText()) {
-            long chat_id = update.getMessage().getChatId();
-            try {
-                execute(new SendMessage(chat_id, "Hi " + update.getMessage().getText()));
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
-    }
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add("Button 3");
+        row2.add("Button 3");
 
-    public void setWebHookPath(String webHookPath) {
-        this.webHookPath = webHookPath;
-    }
-
-    public void setBotUserName(String botUserName) {
-        this.botUserName = botUserName;
-    }
-
-    public void setBotToken(String botToken) {
-        this.botToken = botToken;
+        List<KeyboardRow> rows = new ArrayList<>();
+        rows.add(row1);
+        rows.add(row2);
+        markup.setKeyboard(rows);
+        return markup;
     }
 }
--- src/main/java/com/home/mytelegram_bot/MyTelegramBotApplication.java
+++ src/main/java/com/home/mytelegram_bot/MyTelegramBotApplication.java
@@ -2,12 +2,19 @@ package com.home.mytelegram_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
 public class MyTelegramBotApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TelegramApiRequestException {
         SpringApplication.run(MyTelegramBotApplication.class, args);
+
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        telegramBotsApi.registerBot(new MyTelegramBot());
     }
 
 }
