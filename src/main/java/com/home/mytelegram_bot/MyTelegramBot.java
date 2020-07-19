bc1933ccb52738830bbdab68c01ed815feac9972
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-07-19 23:48:47 +0300

    added method currentTime

--- src/main/java/com/home/mytelegram_bot/MyTelegramBot.java
+++ src/main/java/com/home/mytelegram_bot/MyTelegramBot.java
@@ -8,11 +8,18 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 public class MyTelegramBot extends TelegramLongPollingBot {
+
+    private static final String WHAT_THE_TIME_REQUEST = "What the time?";
+    private static final String WHAT_THE_DATE_REQUEST = "What the date?";
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -20,7 +27,7 @@ public class MyTelegramBot extends TelegramLongPollingBot {
 
 
         try {
-            execute(response);
+            execute(getResponseMessage(message));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
@@ -32,6 +39,7 @@ public class MyTelegramBot extends TelegramLongPollingBot {
                 "You have send me: " + message.getText());
         response.setChatId(message.getChatId());
         response.setReplyMarkup(getMainMenu());
+        return response;
     }
 
     @Override
@@ -41,17 +49,17 @@ public class MyTelegramBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "<botToken>";
+        return "token";
     }
 
     private ReplyKeyboardMarkup getMainMenu() {
         ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
         KeyboardRow row1 = new KeyboardRow();
-        row1.add("Button 1");
-        row1.add("Button 2");
+        row1.add(WHAT_THE_DATE_REQUEST);
+        row1.add(WHAT_THE_TIME_REQUEST);
 
         KeyboardRow row2 = new KeyboardRow();
-        row2.add("Button 3");
+        row2.add("Zhopka,I LOVE YOU:))");
         row2.add("Button 3");
 
         List<KeyboardRow> rows = new ArrayList<>();
@@ -59,5 +67,21 @@ public class MyTelegramBot extends TelegramLongPollingBot {
         rows.add(row2);
         markup.setKeyboard(rows);
         return markup;
+    }
+    private SendMessage getCurrentTimeResponse(Message message){
+        SendMessage response = new SendMessage();
+        response.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
+        response.setChatId(message.getChatId());
+        response.setReplyMarkup(getMainMenu());
+        return response;
+    }
+
+    private SendMessage getResponseMessage(Message message){
+        switch (message.getText()){
+            case WHAT_THE_TIME_REQUEST:
+                return getCurrentTimeResponse(message);
+            default:
+                return greetingMessage(message);
+        }
     }
 }
